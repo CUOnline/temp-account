421b3a75439d222e1b58dc8f7c3d90d7dd6660b7
Connor Ford <connor.ford@ucdenver.edu>
2016-08-26 17:11:22 -0600

    Refactor some helper methods to improve testability

--- controllers/register_controller.rb
+++ controllers/register_controller.rb
@@ -7,21 +7,28 @@ class RegisterController < ApplicationController
   end
 
   post '/' do
+    form do
+      field :full_name, :present => true, :length => 0..255
+      field :email, :present => true, :regexp => /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+      field :expire_in, :present => true, :int => {:gte => 5, :lte => 180}
+    end
+
     begin
-      validate_form(params)
+      validate_form(params, form)
       user = TempAccount::User.new(canvas_api)
       user.register(params['full_name'], params['email'], settings.canvas_account_id)
       user.set_custom_data(user.merge_code)
-      user.setup_sandbox(sandbox_account_id)
+      user.setup_sandbox(get_sandbox_account_id)
     rescue TempAccount::RegistrationError, TempAccount::ApiError => e
       status 400
       flash.now[:danger] = e.message
       return fill_in_form(erb :register)
     end
 
-    queue_workers(params['expire_in'], user)
+    merge_link = merge_link(user.merge_code)
+    queue_workers(params['expire_in'], user.canvas_id, merge_link)
     send_mail('Your temporary Canvas account has been created',
-              registration_body(params['expire_in'], user.merge_code),
+              registration_body(params['expire_in'], merge_link),
               params['email'])
 
     flash.now[:success] = 'Account created successfully.'
--- helpers/register_helper.rb
+++ helpers/register_helper.rb
@@ -2,13 +2,7 @@ require File.expand_path(File.join(File.dirname(__FILE__), '../workers/expiratio
 require File.expand_path(File.join(File.dirname(__FILE__), '../workers/reminder_worker.rb'))
 
 module RegisterHelper
-  def validate_form(params)
-    form do
-      field :full_name, :present => true, :length => 0..255
-      field :email, :present => true, :regexp => /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
-      field :expire_in, :present => true, :int => {:gte => 5, :lte => 180}
-    end
-
+  def validate_form(params, form)
     if form.failed?
       message = ''
       form.failed_fields.each do |f|
@@ -19,24 +13,23 @@ module RegisterHelper
     end
   end
 
-  def queue_workers(expire_in_days, user)
+  def queue_workers(expire_in_days, canvas_id, merge_link)
     remind_in_days = (expire_in_days.to_i - 3)
     remind_in_secs = remind_in_days * 24 * 60 * 60
-    Resque.enqueue_in(remind_in_secs, ReminderWorker, user.canvas_id,
-                      merge_link(user.merge_code))
+    Resque.enqueue_in(remind_in_secs, ReminderWorker, canvas_id, merge_link)
 
     expire_in_secs = expire_in_days.to_i * 24 * 60 * 60
-    Resque.enqueue_in(expire_in_secs, ExpirationWorker, user.canvas_id)
+    Resque.enqueue_in(expire_in_secs, ExpirationWorker, canvas_id)
   end
 
-  def registration_body(expire_in, code)
+  def registration_body(expire_in, merge_link)
     "Your temporary Canvas account will expire in #{expire_in} days. "\
     "Once you get your official Canvas account, visit the link below "\
     "to merge in content from your temporary account. \n"\
-    "<a href='#{merge_link(code)}'>Merge Accounts</a>"
+    "<a href='#{merge_link}'>Merge Accounts</a>"
   end
 
-  def sandbox_account_id
+  def get_sandbox_account_id
     if settings.respond_to?(:sandbox_account_id)
       sandbox_account_id = settings.sandbox_account_id
     else
